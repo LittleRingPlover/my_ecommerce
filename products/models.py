58d4d0eec70253210facb5ebd2163c4b5ed409fa
albrz <al.brzostowicz@gmail.com>
2022-11-30 14:38:09 +0100

    Push project

---
+++ products/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
+
