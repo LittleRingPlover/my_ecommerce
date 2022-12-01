a3a4851f65121c138d7c6408cb9ce917035c8ef1
albrz <al.brzostowicz@gmail.com>
2022-12-01 10:55:47 +0100

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'is_published', 'created_at',)
+    list_display_links = ('id', 'name')
+    list_filter = ('price', 'created_at')
+    list_editable = ('is_published',)
+    search_fields = ('name', 'price')
+    ordering = ('price',)
+
+
+admin.site.register(Product, ProductAdmin)
